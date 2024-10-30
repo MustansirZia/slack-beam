7a06f4d814a495bdbea3fa1357f70dcdb4802aa5
MustansirZia <mustansir29@hotmail.com>
2024-10-30 11:51:24 +0530

    "feat: make llm model configurable"

--- app/language_models/anthropic.py
+++ app/language_models/anthropic.py
@@ -1,5 +1,7 @@
+import os
+
 from langchain_anthropic import ChatAnthropic
 
-model = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+model = ChatAnthropic(model=os.environ.get('ANTHROPIC_MODEL_NAME', 'claude-3-5-sonnet-20241022'))
 
 
